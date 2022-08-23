2f6c68383e32bf31aa9de335d6c53252c130d415
Rino Malbašić <rmalbasic@unipu.hr>
2022-08-23 21:03:17 +0200

    Početak rada

---
+++ src/components/SignUp.vue
@@ -0,0 +1,26 @@
+<template>
+<SignUp/>
+<img class="logo" src="../assets/logo.png">
+<header>
+<h1>Sign up</h1>
+</header>
+<div>
+    <input type="text" placeholder="Enter Name" />
+    <input type="text" placeholder="Enter Email" />
+    <input type="text" placeholder="Enter Password" />
+    <button>Sign Up</button>
+</div>
+</template>
+
+
+<script>
+export default {
+    name: "SignUp",
+}
+</script>
+
+<style>
+.logo{
+    width: 100px;
+}
+</style>
